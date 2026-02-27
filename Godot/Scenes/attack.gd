ba6713b3a742bf4318a47897053acbf846a2baf4
Hojun Yeon <57352761+ghwns977@users.noreply.github.com>
2026-02-27 15:31:01 -0700

    godot update

--- Godot/Scenes/attack.gd
+++ Godot/Scenes/attack.gd
@@ -10,3 +10,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Attack"):
 		attack_held = true
 		print("ATTACK PRESSED")
+	
+	if Input.is_action_just_released("Attack"):
+		attack_held = false
+		print("ATTACK RELEASED")
+		
+	if attack_held:
+		print("ATTACK HELD")
+		
+	var move := Input.get_vector("move_left","move_right","move_up","move_down")
+	if move != Vector2.ZERO:
+		print("MOVE ", move)
